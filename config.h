2e33e6cf3fa6b9f9e0bed02aba3432577c616363
cultab <rroarck@gmail.com>
2024-11-03 20:00:59 +0200

    fix: use group that is allowed to verify
    Specifically by polkit rule net.reactivated.fprint.device.verify
    see: dotfiles/misc/50-fprintd.rules

--- config.h
+++ config.h
@@ -1,6 +1,6 @@
 /* user and group to drop privileges to */
 static const char *user  = "evan";
-static const char *group = "evan";
+static const char *group = "plugdev";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
